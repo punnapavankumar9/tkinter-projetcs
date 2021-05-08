e877a85725c59335cdb19ba671869ca0e8b8c3e6
pavan kumar punna <67155391+punnapavankumar9@users.noreply.github.com>
2021-05-09 00:18:16 +0530

    added opening a saved file and changed readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # tkinter-projetcs
+- Text editor using tkinter
+- Scientific calculator
+- Paint used for drawing images and saving in jpg form
--- paint/painter.py
+++ paint/painter.py
@@ -1,8 +1,8 @@
 import tkinter as tk
 from tkinter.colorchooser import askcolor
-from paint.MyDialog import MyDialog
+from MyDialog import MyDialog
 from tkinter import filedialog
-from PIL import Image
+from PIL import Image, ImageTk
 import os
 import io
 
@@ -15,8 +15,6 @@ class Painter:
         self.root = tk.Tk()
         self.root.geometry('500x500')
         self.root.title('Paint by punna')
-        # menu bar
-
         self.setup_options_frame()
         self.setup_menubar()
         self.setup_canvas()
@@ -24,13 +22,13 @@ class Painter:
         self.binding()
         self.root.mainloop()
 
-    # setting up the canvas to draw and erase
+    # configuresb the options frame below the menu
     def setup_options_frame(self):
         self.options_frame = tk.Frame(self.root, width=500, height=55, relief=tk.RIDGE, borderwidth=2, bg="#CCC")
         self.options_frame.place(x=0, y=0)
         self.pen_func()
-        self.eraiser_func()
-        self.enable_draw_func()
+        self.eraiser_btn()
+        self.enable_draw_btn()
 
     def pen_func(self):
         self.PEN_SIZE_VAR = tk.DoubleVar()
@@ -41,22 +39,26 @@ class Painter:
         self.pen_size_label.grid(row=0, column=0)
         self.pen.grid(row=0, column=1)
 
-    def eraiser_func(self):
+    def eraiser_btn(self):
         self.eraiserlabel = tk.Button(self.options_frame, text='Eraser', bg='#ccc', padx=10, pady=5 , command=self.set_erase_mode)
         self.eraiserlabel.grid(row=0, column=2)
 
-    def enable_draw_func(self):
+    def enable_draw_btn(self):
         self.drawlabel = tk.Button(self.options_frame, text='Draw', bg='#ccc', padx=10, pady=5, command=self.enable_draw)
         self.drawlabel.grid(row=0, column=3)
 
+    # menu bar options setup
     def setup_menubar(self):
         self.menu_bar = tk.Menu(self.root)
         self.file_menu = tk.Menu(self.menu_bar, tearoff=0)
-        self.file_menu.add_command(label="choose color", accelerator='Shift+E', command=self.choose_color)
+        self.file_menu.add_command(label="choose color", accelerator='Shift+D', command=self.choose_color)
         self.file_menu.add_command(label="Eraser", accelerator='Ctrl+E', command=self.set_erase_mode)
         self.file_menu.add_command(label="Change Sheet size", accelerator='Ctrl+K', command=self.set_canvas_props_window)
+        self.file_menu.add_command(label="Open file", accelerator='Ctrl+N', command=self.open_file)
+        self.file_menu.add_command(label="Save file", accelerator='Ctrl+S', command=self.save)
         self.menu_bar.add_cascade(label="Options", menu=self.file_menu)
 
+    # setting up the canvas
     def setup_canvas(self, height=500, width=500):
         self.cvs = tk.Canvas(self.root, borderwidth=0, bg='white', height=height, width=width, relief=tk.RIDGE)
         self.cvs.place(x=0, y=53)
@@ -85,6 +87,7 @@ class Painter:
     def enable_draw(self, *args):
         self.erase_mode = False
 
+    # setting up the canvas to draw and erase   
     def draw(self, event):
         self.line_width = self.PEN_SIZE_VAR.get()
         paint_color = 'white' if self.erase_mode else self.color
@@ -97,7 +100,8 @@ class Painter:
     def reset(self, *args, **kwargs):
         self.old_x, self.old_y = None, None
 
-    def save(self, event):
+    # saving drawn canvas to image file
+    def save(self, *args):
         global url
 
         try:
@@ -114,14 +118,31 @@ class Painter:
         except Exception as e:
             print(e)
             return None
+    # opening a file using filedialog box
+    def open_file(self, *args):
+        global url
+        try:
+            url = filedialog.askopenfile(mode='r', filetypes=(('jpeg files', '*.jpg'), ('png files', '*.png'))).name
+            print(url)
+            self.img = Image.open(url)
+            self.set_canvas_props(self.img.size[0], self.img.size[1])
+            k = self.img.size
+            print(self.img.size)
+            self.img = ImageTk.PhotoImage(self.img)
+            self.cvs.create_image(k[0]//2, k[1]//2, image=self.img)
 
+        except Exception as e:
+            print(e)
 
+    # binding key for shortcuts
     def binding(self):
         self.root.bind('<Control-e>', self.set_erase_mode)
         self.root.bind('<Control-s>', self.save)
+        self.root.bind('<Control-n>', self.open_file)
         self.root.bind('<Shift-D>', self.choose_color)
         self.root.bind('<Control-k>', self.set_canvas_props_window)
         self.root.bind('<Control-d>', self.enable_draw)
+
 
 if __name__ == '__main__':
     Painter()
